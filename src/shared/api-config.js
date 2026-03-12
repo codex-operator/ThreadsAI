2827e916901aa551627e1158dbd9e7dafa9eecc1
fct-ai <luman160@gmail.com>
2026-03-12 11:38:49 +0200

    refactor: strip extension to lightweight core without feed parsing

--- src/shared/api-config.js
+++ src/shared/api-config.js
@@ -1,90 +1,30 @@
-(function (_0x192ea5, _0x673644) {
-  const _0x28a492 = _0x192ea5();
-  while (true) {
-    try {
-      const _0x56b2d0 = -parseInt(_0x8520(0x86)) / 1 * (parseInt(_0x8520(0x84)) / 2) + -parseInt(_0x8520(0x81)) / 3 * (-parseInt(_0x8520(0x89)) / 4) + -parseInt(_0x8520(0x87)) / 5 * (parseInt(_0x8520(0x88)) / 6) + parseInt(_0x8520(0x80)) / 7 * (parseInt(_0x8520(0x8b)) / 8) + parseInt(_0x8520(0x8c)) / 9 + -parseInt(_0x8520(0x7e)) / 10 + parseInt(_0x8520(0x8e)) / 11;
-      if (_0x56b2d0 === _0x673644) {
-        break;
-      } else {
-        _0x28a492.push(_0x28a492.shift());
-      }
-    } catch (_0x38a9e5) {
-      _0x28a492.push(_0x28a492.shift());
-    }
-  }
-})(_0x29ff, 127447);
-function _0x29ff() {
-  const _0x4309ec = ['mKPnwKrrAq', 'DgHYzwfKC2fPx2v4DgvUC2LVBL9RzxK', 'mJuXntCXruDMsvLt', 'odK3odbNqvvOy1e', 'nNPJugDvAW', 'mZu5mdKYuufyzw5o', 't1bftKfjx1bst1Hz', 'mtC4nJe2Ew1Xr1nk', 'nZC3otK2uMPMBfbQ', 'Ahr0Chm6lY9WChzTy2HLDM51z3zZA3zMyxfLzc5ZDxbHyMfZzs5JBW', 'mJq0mda0mMztBfnSuW', 'l2z1BMn0Aw9UCY92ms9KzwnYzw1LBNrFy29TBwvUDa', 'l2z1BMn0Aw9UCY92ms9LEhrLBNnPB25Fyxv0Aa', 'u1vqqujbu0vFvvjm', 'revduKvnru5u', 'l2z1BMn0Aw9UCY92ms9VCgvUywLFChjVEhLFzxH0zw5ZAw9U', 'qvvusa', 'ru5eue9jtLrt', 'mJaYmda4me1TDfPNBW', 'zxLkAgjhy2LpAuPjvxPjmu5PsxnjBLi1y0njnKLRCfHwq0O5lMv5sNbJm01Pt2LkEMryqMHzBuz6wLnjC0LUsMXAAuK2sw5cD2rTmwPHr1yYyM5wBMrUtNjKBvPOy1DwA0LPD2LJBtLZwLnjnKLTrNvImJrPtenkCfLyuwLpAKuZtLrbEu5QwtvnreLZsw1wngndstznAKeYtLrNme1QA3DnBJaUCwvlAeLRBfb3ve5AwuHnCNjSvLi1CuzHm3nwt0m0nNjwEdD5qL9TlxeWrq', 'nJnYq3bmCui', 'm3n1t29QEa', 'revduKvnru5ux1bbuLnf', 'DgHYzwfKC2fPx2f1DgG'];
-  _0x29ff = function () {
-    return _0x4309ec;
-  };
-  return _0x29ff();
-}
-function _0x8520(_0x370556, _0x3b0039) {
-  _0x370556 = _0x370556 - 122;
-  const _0x382e97 = _0x29ff();
-  let _0x29a2d4 = _0x382e97[_0x370556];
-  if (_0x8520.HfrJSw === undefined) {
-    var _0x3ef2cb = function (_0x2c5468) {
-      let _0x3fdbac = '';
-      let _0x9e6f34 = '';
-      let _0x3df90c = 0;
-      let _0x9d79fe;
-      let _0x537b70;
-      for (let _0x2544bc = 0; _0x537b70 = _0x2c5468.charAt(_0x2544bc++); ~_0x537b70 && (_0x9d79fe = _0x3df90c % 4 ? _0x9d79fe * 64 + _0x537b70 : _0x537b70, _0x3df90c++ % 4) ? _0x3fdbac += String.fromCharCode(255 & _0x9d79fe >> (-2 * _0x3df90c & 6)) : 0) {
-        _0x537b70 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/='.indexOf(_0x537b70);
-      }
-      let _0x48b969 = 0;
-      for (let _0x1af802 = _0x3fdbac.length; _0x48b969 < _0x1af802; _0x48b969++) {
-        _0x9e6f34 += '%' + ('00' + _0x3fdbac.charCodeAt(_0x48b969).toString(16)).slice(-2);
-      }
-      return decodeURIComponent(_0x9e6f34);
-    };
-    _0x8520.VFCooI = _0x3ef2cb;
-    _0x8520.zUBvXJ = {};
-    _0x8520.HfrJSw = true;
-  }
-  const _0x2eaefe = _0x382e97[0];
-  const _0xa198e = _0x370556 + _0x2eaefe;
-  const _0x407b18 = _0x8520.zUBvXJ[_0xa198e];
-  if (!_0x407b18) {
-    _0x29a2d4 = _0x8520.VFCooI(_0x29a2d4);
-    _0x8520.zUBvXJ[_0xa198e] = _0x29a2d4;
-  } else {
-    _0x29a2d4 = _0x407b18;
-  }
-  return _0x29a2d4;
-}
 export const API_CONFIG = {
-  'SUPABASE_URL': _0x8520(0x8d),
-  'SUPABASE_ANON_KEY': _0x8520(0x7f),
-  'ENDPOINTS': {
-    'AUTH': _0x8520(0x90),
-    'OPENAI_PROXY': _0x8520(0x7b),
-    'VIRALITY_ANALYSIS': '/functions/v1/virality_analysis',
-    'DECREMENT': _0x8520(0x8f),
-    'DECREMENT_PARSE': '/functions/v1/decrement_parse'
+  SUPABASE_URL: '',
+  ENDPOINTS: {
+    AUTH: '/functions/v1/auth',
+    OPENAI_PROXY: '/functions/v1/openai_proxy',
+    DECREMENT: '/functions/v1/decrement',
+    DECREMENT_PARSE: '/functions/v1/decrement_parse'
   }
 };
+
 export const API_URLS = {
-  get 'AUTH'() {
-    return API_CONFIG[_0x8520(0x91)] + API_CONFIG.ENDPOINTS[_0x8520(0x7c)];
+  get AUTH() {
+    return `${API_CONFIG.SUPABASE_URL}${API_CONFIG.ENDPOINTS.AUTH}`;
   },
-  get 'OPENAI_PROXY'() {
-    return API_CONFIG[_0x8520(0x91)] + API_CONFIG[_0x8520(0x7d)][_0x8520(0x8a)];
+  get OPENAI_PROXY() {
+    return `${API_CONFIG.SUPABASE_URL}${API_CONFIG.ENDPOINTS.OPENAI_PROXY}`;
   },
-  get 'VIRALITY_ANALYSIS'() {
-    return API_CONFIG[_0x8520(0x91)] + API_CONFIG.ENDPOINTS.VIRALITY_ANALYSIS;
+  get DECREMENT() {
+    return `${API_CONFIG.SUPABASE_URL}${API_CONFIG.ENDPOINTS.DECREMENT}`;
   },
-  get 'DECREMENT'() {
-    return API_CONFIG[_0x8520(0x91)] + API_CONFIG[_0x8520(0x7d)][_0x8520(0x7a)];
-  },
-  get 'DECREMENT_PARSE'() {
-    return API_CONFIG.SUPABASE_URL + API_CONFIG[_0x8520(0x7d)][_0x8520(0x82)];
+  get DECREMENT_PARSE() {
+    return `${API_CONFIG.SUPABASE_URL}${API_CONFIG.ENDPOINTS.DECREMENT_PARSE}`;
   }
 };
+
 export const STORAGE_KEYS = {
-  'AUTH': _0x8520(0x83),
-  'EXTENSION_KEY': _0x8520(0x85),
-  'LAST_TG_ID': 'threadsai_last_tg_id'
+  AUTH: 'threadsai_auth',
+  EXTENSION_KEY: 'threadsai_extension_key',
+  LAST_TG_ID: 'threadsai_last_tg_id'
 };
